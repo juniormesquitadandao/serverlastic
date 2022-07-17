68914643c9f0ba38b2a7a959c968e6253e59bf96
juniormesquitadandao <juniormesquitadandao@gmail.com>
2022-07-17 16:27:56 -0300

    sp3

--- install.sh
+++ install.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-rm -rf /tmp/serverlastic.zip /tmp/serverlastic tmp/sam tmp/Dockerfile.sam
+rm -rf /tmp/serverlastic.zip /tmp/serverlastic sam Dockerfile.sam
 
 wget https://github.com/juniormesquitadandao/serverlastic/archive/refs/heads/main.zip -q --show-progress -O /tmp/serverlastic.zip
 
 unzip -q -d /tmp/serverlastic /tmp/serverlastic.zip
 
-mv /tmp/serverlastic/serverlastic-main/sam tmp/
+mv /tmp/serverlastic/serverlastic-main/sam .
 
-mv /tmp/serverlastic/serverlastic-main/Dockerfile.sam tmp/
+mv /tmp/serverlastic/serverlastic-main/Dockerfile.sam .
 
-sed "s/YOUR_SERVER_IMAGE/$(basename $PWD)_app/g" -i tmp/sam/template.yaml
-sed "s/YOUR_FUNCTION_NAME/$(basename $PWD)_function/g" -i tmp/sam/template.yaml
+sed "s/YOUR_SERVER_IMAGE/$(basename $PWD)_app/g" -i sam/template.yaml
+sed "s/YOUR_FUNCTION_NAME/$(basename $PWD)_function/g" -i sam/template.yaml
 
-sed "s/YOUR_STACK_NAME/$(basename $PWD)_function_stack/g" -i tmp/sam/config.toml
-sed "s/YOUR_S3_PREFIX/$(basename $PWD)_function/g" -i tmp/sam/config.toml
+sed "s/YOUR_STACK_NAME/$(basename $PWD)_function_stack/g" -i sam/config.toml
+sed "s/YOUR_S3_PREFIX/$(basename $PWD)_function/g" -i sam/config.toml
 
 echo 'Stack Name? ENTER'
 echo 'AWS Region? ENTER'
@@ -28,7 +28,7 @@ echo 'Save arguments to configuration file? Y'
 echo 'SAM configuration file [../sam/config.toml]? ENTER'
 echo 'SAM configuration environment? ENTER'
 
-chmod +x tmp/sam/devops/**/*.sh
+chmod +x sam/devops/**/*.sh
 
 ./sam/devops/lambda/deploy.sh --guided
 
