d5a5ee84c8b972440dbeb95900646dc840875d7b
juniormesquitadandao <juniormesquitadandao@gmail.com>
2022-07-18 01:00:32 -0300

    ruby on rails

--- projects/ruby_on_rails/sam/devops/server/start.sh
+++ projects/ruby_on_rails/sam/devops/server/start.sh
@@ -2,21 +2,15 @@
 
 set -e
 
-TMP_DIR=/tmp/serverlastic
+cd /tmp/serverlastic
 
-if ! test -d $TMP_DIR; then
-  unzip -q /home/user/serverlastic.zip -d /tmp
+  if [ "${NODE_ENV}" = 'test' ]; then
+    PORT=80
+  else
+    PORT=9000
+  fi
 
-  cd $TMP_DIR
-
-    if [ "${NODE_ENV}" = 'test' ]; then
-      PORT=80
-    else
-      PORT=9000
-    fi
-
-    RAILS_ENV=production \
-      RAILS_MAX_THREADS=1 \
-      RAILS_SERVE_STATIC_FILES=true \
-      rails s -b '127.0.0.1' --port $PORT
-fi
+  RAILS_ENV=production \
+    RAILS_MAX_THREADS=1 \
+    RAILS_SERVE_STATIC_FILES=true \
+    rails s -b '127.0.0.1' --port $PORT
