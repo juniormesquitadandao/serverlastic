5689c972e531a27dc51f0f4c3796f502e2fe8864
Marcelo Junior <juniormesquitadandao@gmail.com>
2022-07-18 00:57:01 -0300

    Update start.sh

--- sam/devops/server/start.sh
+++ sam/devops/server/start.sh
@@ -2,18 +2,12 @@
 
 set -e
 
-TMP_DIR=/tmp/serverlastic
+cd /tmp/serverlastic
 
-if ! test -d $TMP_DIR; then
-  unzip -q /home/user/serverlastic.zip -d /tmp
+  if [ "${NODE_ENV}" = 'test' ]; then
+    PORT=80
+  else
+    PORT=9000
+  fi
 
-  cd $TMP_DIR
-
-    if [ "${NODE_ENV}" = 'test' ]; then
-      PORT=80
-    else
-      PORT=9000
-    fi
-
-    node_modules/http-server/bin/http-server app -a 127.0.0.1 --port $PORT
-fi
+  node_modules/http-server/bin/http-server app -a 127.0.0.1 --port $PORT
